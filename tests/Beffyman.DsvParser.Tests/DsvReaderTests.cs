d4d2dac4e7ff2adbfad1d15f1de80ab5e79aeaf7
Connor Laderer <splashy5@hotmail.com>
2019-04-08 22:29:11 -0400

    DataHasLessColumnsThanHeader

--- tests/Beffyman.DsvParser.Tests/DsvReaderTests.cs
+++ tests/Beffyman.DsvParser.Tests/DsvReaderTests.cs
@@ -312,6 +312,41 @@ namespace Beffyman.DsvParser.Tests
 		}
 
 		[Fact]
+		public void DataHasLessColumnsThanHeader()
+		{
+			string file = $"Column1,Column2,Column3{Environment.NewLine}Data1,Data2";
+
+			var data = new DsvReader(file, DsvOptions.DefaultCsvOptions);
+
+			List<ReadOnlyMemory<char>> columns = null;
+			List<List<ReadOnlyMemory<char>>> rows = new List<List<ReadOnlyMemory<char>>>();
+
+
+			while (data.MoveNext())
+			{
+				if (!data.ColumnsFilled)
+				{
+					columns = data.ReadLine().ToList();
+				}
+				else
+				{
+					rows.Add(data.ReadLine().ToList());
+				}
+			}
+
+			Assert.Equal(3, columns.Count);
+			Assert.Single(rows);
+
+			Assert.Equal("Column1", columns[0].ToString());
+			Assert.Equal("Column2", columns[1].ToString());
+			Assert.Equal("Column3", columns[2].ToString());
+
+			Assert.Equal("Data1", rows[0][0].ToString());
+			Assert.Equal("Data2", rows[0][1].ToString());
+			Assert.Equal("", rows[0][2].ToString());
+		}
+
+		[Fact]
 		public void MultipleLinesBetweenData()
 		{
 			string file = $"Column1,Column2{Environment.NewLine}{Environment.NewLine}Data1,Data2{Environment.NewLine}{Environment.NewLine}Data1,Data2";
